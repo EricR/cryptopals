b0f1cc235bd1c39d0eec87649b929700788cc317
Eric Rafaloff <erafaloff@gmail.com>
2018-12-30 22:50:33 -0500

    Solved challenge 14

--- challenge_12.py
+++ challenge_12.py
@@ -8,19 +8,24 @@ import unittest
 import challenge_7
 import challenge_11
 
-secret = base64.b64decode("Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd2" +
-    "4gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdX" +
-    "N0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK")
+# secret = base64.b64decode("Um9sbGluJyBpbiBteSA1LjAKV2l0aCBteSByYWctdG9wIGRvd2" +
+#     "4gc28gbXkgaGFpciBjYW4gYmxvdwpUaGUgZ2lybGllcyBvbiBzdGFuZGJ5IHdhdmluZyBqdX" +
+#     "N0IHRvIHNheSBoaQpEaWQgeW91IHN0b3A/IE5vLCBJIGp1c3QgZHJvdmUgYnkK")
+secret = bytes("test", 'ascii')
 
 class Challenge12(unittest.TestCase):
-    def test_plaintext_recovery(self):
+    def test_determine_block_stats(self):
         b_size, pt_size, padding = determine_block_stats(encryption_oracle)
         self.assertEqual(b_size, 16)
         self.assertEqual(pt_size, len(secret))
         self.assertEqual(padding, 6)
-        mode = detect_mode(encryption_oracle, b_size)
+
+    def test_determine_mode(self):
+        mode = detect_mode(encryption_oracle)
         self.assertEqual(mode, 'ecb')
-        plaintext = recover_plaintext(encryption_oracle, pt_size, b_size)
+
+    def test_plaintext_recovery(self):
+        plaintext = recover_plaintext(encryption_oracle)
         self.assertEqual(plaintext, secret)
 
 def deterministic_random_key():
@@ -48,7 +53,7 @@ def determine_block_stats(oracle):
         else:
             padding += 1
 
-def recover_plaintext(oracle, text_size, block_size):
+def recover_plaintext(oracle):
     """
     Exploits the fact that ECB mode is used and that we can prepend data to the
     unknown plaintext before encryption. By intentionally padding the unknown
@@ -59,9 +64,10 @@ def recover_plaintext(oracle, text_size, block_size):
     each block until the entire ciphertext is decrypted.
     """  
     plaintext = bytearray()
+    block_size, text_size, _ = determine_block_stats(encryption_oracle)
 
     for i in range(1, text_size+1):
-        # Pad so that encrypt(padding || recovered plaintext) leaves only
+        # Pad so that AES-128-ECB(padding || recovered plaintext) leaves only
         # one unknown byte in the current block at a time
         padding_len = (block_size - i) % block_size
         padding = bytes('A' * padding_len, 'ascii')
@@ -76,15 +82,12 @@ def recover_plaintext(oracle, text_size, block_size):
             guess = oracle(padding + plaintext + n)[block_start:block_end]
             if guess == block:
                 plaintext += n
-                print("Recovered {}/{}: {}".format(i, text_size, bytes(plaintext)),
-                    end="\r")
                 break
 
-    print("")
     return bytes(plaintext)
 
-def detect_mode(oracle, block_size):
-    plaintext = bytes("A" * 64, 'ascii')
+def detect_mode(oracle):
+    plaintext = bytes("A" * 128, 'ascii')
     return challenge_11.detect_mode(oracle(plaintext))
 
 if __name__ == '__main__':
---
+++ challenge_14.py
@@ -0,0 +1,88 @@
+# Challenge 14 - Byte-at-a-time ECB decryption (Harder)
+#
+# https://cryptopals.com/sets/2/challenges/14
+
+import random
+import unittest
+import challenge_7
+import challenge_11
+import challenge_12
+
+class Challenge14(unittest.TestCase):
+    def test_determine_offset(self):
+        offset = determine_offset(encryption_oracle, 16)
+        self.assertEqual(offset, len(deterministic_random_bytes()))
+
+    def test_plaintext_recovery(self):
+        plaintext = recover_plaintext(encryption_oracle)
+        self.assertEqual(plaintext, challenge_12.secret)
+        return True
+
+def deterministic_random_bytes():
+    random.seed(123)
+    size = random.randint(100,1000)
+    return bytes([random.getrandbits(8) for _ in range(size)])
+
+def encryption_oracle(plaintext):
+    key = challenge_12.deterministic_random_key()
+    prefix = deterministic_random_bytes()
+    return challenge_11.AES_ECB(key).encrypt(prefix + plaintext
+        + challenge_12.secret)
+
+def determine_offset(oracle, block_size):
+    """
+    Determines the offset in which user input starts within the ciphertext by
+    analyzing what injected padding causes a repeated block.
+    """
+    original_ciphertext = oracle(bytes())
+
+    for i in range(1, 128):
+        ciphertext = oracle(bytes('A', 'ascii') * i)
+        blocks = challenge_7.as_blocks(ciphertext, block_size)
+        last = None
+
+        for idx, block in enumerate(blocks):
+            if block == last:
+                return idx * block_size - (i - block_size)
+            last = block
+
+def recover_plaintext(oracle):
+    """
+    Implements an attack similar to that in challenge 12, but accounts for the
+    fact that some unknown characters are now inserted before any user input
+    (referred to as an "offset" below).
+    """  
+    plaintext = bytearray()
+    block_size, text_size, _ = challenge_12.determine_block_stats(oracle)
+    offset = determine_offset(oracle, block_size)
+    extra_blocks = offset // block_size
+
+    # Account for an extra block when the offset isn't perfectly aligned with
+    # the block size
+    if offset % block_size != 0:
+        extra_blocks += 1
+
+    extra_padding = extra_blocks * block_size - offset
+
+    for i in range(1, text_size-offset+1):
+        # Pad so that AES-128-ECB(random-prefix || padding || recovered plaintext)
+        # leaves only one unknown byte in the current block at a time
+        padding_len = (block_size - i) % block_size + extra_padding
+        padding = bytes('A' * padding_len, 'ascii')
+
+        # Keep track of which block we're working on
+        block_n = len(plaintext) // block_size + extra_blocks
+        block_start = block_n * block_size
+        block_end = block_start + block_size
+        block = oracle(padding)[block_start:block_end]
+
+        # Guess the unknown byte (n)
+        for n in [bytes([n]) for n in range(256)]:
+            guess = oracle(padding + plaintext + n)[block_start:block_end]
+            if guess == block:
+                plaintext += n
+                break
+    return plaintext
+
+if __name__ == '__main__':
+    unittest.main()
