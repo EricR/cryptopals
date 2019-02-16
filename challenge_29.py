be05946653bb6b919c51f61c6d2817799bebd87a
Eric Rafaloff <erafaloff@gmail.com>
2019-02-16 08:34:04 -0500

    WIP

--- challenge_29.py
+++ challenge_29.py
@@ -5,8 +5,8 @@
 import challenge_28
 
 
-def generate_potential_extensions(orig, extension, possible_lens=range(0, 12)):
-    digest = challenge_28.SHA1(b"A" * 64)
+def generate_sha1_extension(orig, extension, length):
+    digest = challenge_28.SHA1(b"A" * length)
     digest.h0 = int(orig[0:4], 16)
     digest.h1 = int(orig[4:8], 16)
     digest.h2 = int(orig[8:12], 16)
@@ -18,6 +18,3 @@ def generate_potential_extensions(orig, extension, possible_lens=range(0, 12)):
         digest._SHA1__process_block(block)
 
     return digest.hexdigest()
-
-print(challenge_28.SHA1(b"asdf").hexdigest())
-print(challenge_28.SHA1(b"1234").hexdigest())
